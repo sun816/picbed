2bb7f3b88f17cb1098ea74838999fb7b4ba6d750
sun816 <sunjoypie@gmail.com>
2023-05-12 12:15:47 +0800

    Upload by PicGo

---
+++ pic/恢复网银进程.bat
@@ -0,0 +1,9 @@
+suspend.exe -r NBCBSignService.exe
+suspend.exe -r NBCBSecInputService.exe
+suspend.exe -r HZBankKzSignServerService.exe
+suspend.exe -r HZBankSecurityInputServiceKz.exe
+suspend.exe -r SPDBSignService.exe
+suspend.exe -r SPDBSecInputServ.exe
+suspend.exe -r NBCBSecInput.exe
+suspend.exe -r SPDBSecInput.exe
+pause
