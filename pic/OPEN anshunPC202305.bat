658e7477e101cb56a8e2a749ca8351f2c3fc6333
sun816 <sunjoypie@gmail.com>
2023-05-12 12:15:41 +0800

    Upload by PicGo

---
+++ pic/OPEN anshunPC202305.bat
@@ -0,0 +1,25 @@
+@echo off   声明
+echo open 文件夹
+
+start "" "C:\cf\bin"
+start "" "C:\Users\Administrator\montastic"
+start "" "C:\Users\Administrator\Documents\GitHub\montastic"
+start "" "D:\202003阅读pdf目录 anshun PC同步"
+start "" "C:\Users\Administrator\Desktop\anshun PC工作备份\2023工作"
+start "" "C:\Users\Administrator\Downloads"
+start "" "C:\Users\Administrator\Desktop\20221020 批量挂起恢复进程-修改hosts进程"
+start "" "C:\Users\Administrator\Desktop\202111 专利 RPL自主网络automonic"
+start "" "C:\Users\Administrator\Desktop\anshun PC工作备份\2023工作\vCPE\vCPE规范及参考"
+start "" "C:\Users\Administrator\Desktop\anshun PC工作备份\WBBA组织"
+start "" "C:\Users\Administrator\Desktop\anshun PC工作备份\6G 202109\6G仿真相关\202205 6G仿真ppt"
+start "" "C:\Users\Administrator\Documents\GitHub"
+start "" "D:\Documents\GitHub\life1"
+start "" "C:\Users\Administrator\Documents\GitHub\life1"
+start "" "C:\cf\bin\.cloudflared"
+start "" "E:\树莓派4B镜像备份"
+start "" "C:\Users\Administrator\Desktop\anshun PC工作备份\IPJ电子版From 2021"
+start "" "E:\VPN翻墙配置\BWG配置"
+start "" "C:\Users\Administrator\AppData\LocalLow\Owlcat Games\Pathfinder Kingmaker"
+start "" "E:\win10 安装"
+
+pause   
