93c87915fe852e9593ff6e10e2c75a4cf09057e4
sun816 <sunjoypie@gmail.com>
2023-05-12 12:15:45 +0800

    Upload by PicGo

---
+++ pic/挂起网银进程.bat
@@ -0,0 +1,9 @@
+suspend.exe NBCBSignService.exe
+suspend.exe NBCBSecInputService.exe
+suspend.exe HZBankKzSignServerService.exe
+suspend.exe HZBankSecurityInputServiceKz.exe
+suspend.exe SPDBSignService.exe
+suspend.exe SPDBSecInputServ.exe
+suspend.exe NBCBSecInput.exe
+suspend.exe SPDBSecInput.exe
+pause
